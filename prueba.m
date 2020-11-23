702edfe30f80163ccbf1deb8202a5d3caf2c6821
Rodolfo <rodo655@hotmail.com>
2020-11-23 11:55:59 -0600

    prueba Octave Script

---
+++ prueba.m
@@ -0,0 +1,28 @@
+## Headline title
+#
+# Some *bold*, _italic_, or |monospaced| Text with
+# a <https://www.octave.org link to *GNU Octave*>.
+##
+
+# "Real" Octave commands to be evaluated
+sombrero ()
+
+%% MATLAB comment style ('%') is supported as well
+%
+% * Bulleted list item 1
+% * Bulleted list item 2
+%
+% # Numbered list item 1
+% # Numbered list item 2
+
+
+
+%% Tìtulo de la práctica
+
+# Desarrollo 
+
+# *Bullet list Actividad 1
+# * Bullet list Actividad 2
+
+solucion = 2 + 3
+matriz = 1:10
