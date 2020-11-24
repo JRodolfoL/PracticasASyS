c74ea155f33edbea4131e3f49253eb8bbbe7d30e
Rodolfo <rodo655@hotmail.com>
2020-11-24 01:07:27 -0600

    Primer avance 1,11

--- prueba.m
+++ prueba.m
@@ -1,28 +1,50 @@
-## Headline title
-#
-# Some *bold*, _italic_, or |monospaced| Text with
-# a <https://www.octave.org link to *GNU Octave*>.
+## Práctica 1: Señales en tiempo continuo.
+%
+% *Integrantes:*
+%
+% * Arellano Paz Angel Ulises
+% * Cardoso Arias Javier
+% * Gachuz Hernández Karla Denisse
+% * García Arteaga Alejandro
+% * López Galván José Rodolfo
+%
 ##
 
-# "Real" Octave commands to be evaluated
-sombrero ()
+%% Objetivos 
+% * Manipulación básica de MATLAB
+% * Gráficas de señales reales y complejas continuas
+% * Transformación de señales continuas (escalamientos y traslaciones)
+% * Calculo de energía y potencia de señales continuas
 
-%% MATLAB comment style ('%') is supported as well
+%%  1.11 Working with functions.
 %
-% * Bulleted list item 1
-% * Bulleted list item 2
+% Working with functions is fundamental to signals and systems applications. MATLAB provides several methods of defining and evaluating functions. An understanding and proficient use of these methods are therefore necessary and beneficial.
+
+%%
+% *Anonymous functions.*
 %
-% # Numbered list item 1
-% # Numbered list item 2
+% Many simple functions are most conveniently represented by using MATLAB anonymous functions. An anonymous function provides a symbolic representation of a function defined in terms of MATLAB operators, functions, or other anonymous functions. For example, consider defining the exponentially damped sinusoid $f (t) = e −t cos(2π t)$.
 
+f = @(t) exp(-t).*cos(2*pi*t);
 
+%%
+% In this context, the @ symbol identifies the expression as an anonymous function, which is assigned a name of f. Parentheses following the @ symbol are used to identify the function’s independent variables (input arguments), which in this case is the single time variable t. Input arguments, such as t, are local to the anonymous function and are not related to any workspace variables with the same names.
+%
+% Once defined, $f (t)$ can be evaluated simply by passing the input values of interest. For example,
 
-%% Tìtulo de la práctica
+t =0; f(t)
 
-# Desarrollo 
+%%
+% evaluates $f (t)$ at t = 0, confirming the expected result of unity. The same result is obtained by passing t = 0 directly.
 
-# *Bullet list Actividad 1
-# * Bullet list Actividad 2
+%%
+% Vector inputs allow the evaluation of multiple values simultaneously, Consider the task of plotting $f(t)$ over the interval $(-2 \leq t \leq 2)$. Gross function behavior is clear:  $f(t)$ should oscilate four times with a decaying envelope. Since accurate hand sketches are cumberstone, MATLAB-generated plots are an attractive alternative. As the following example illustrates, care must be taken to ensure reliable results.
+%
+% Suppose the vector t is chosen to include onlu the integers contained in $(-2 \leq t \leq 2)$, namely, [−2, −1, 0, 1, 2].
 
-solucion = 2 + 3
-matriz = 1:10
+t = (-2:2);
+
+%%
+% This vector input is evaluated from a vector output.
+
+f(t)
